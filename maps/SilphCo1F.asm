fa6cf3f754ffbc3154e6a33aeeade84868075d33
i-am-the-pokeman <joseph.r.bayer@gmail.com>
2025-08-22 22:12:05 -0500

    Add Charlie, the mystery gift  researcher to Silph Co

--- maps/SilphCo1F.asm
+++ maps/SilphCo1F.asm
@@ -1,6 +1,7 @@
 	object_const_def
 	const SILPHCO1F_RECEPTIONIST
 	const SILPHCO1F_OFFICER
+  const SILPHCO1F_CHARLIE
 
 SilphCo1F_MapScripts:
 	def_scene_scripts
@@ -26,6 +27,19 @@ SilphCoOfficerScript:
 .NoRoom:
 	closetext
 	end
+
+SilphCoCharlieScript:
+  faceplayer
+  opentext
+  writetext SilphCoCharlieText_OfferMysteryGiftText
+  waitbutton
+  closetext
+  domysterygift NPC_MYSTERY_GIFT_NPC_CHARLIE
+  opentext
+  writetext SilphCoCharlieText_ExitText
+  waitbutton
+  closetext
+  end
 
 SilphCoReceptionistText:
 	text "Welcome. This is"
@@ -53,6 +67,22 @@ SilphCoOfficerText_GotUpGrade:
 	line "anywhere yet."
 	done
 
+SilphCoCharlieText_OfferMysteryGiftText:
+  text "The mysteries of"
+  line "MYSTERY GIFT..."
+
+  para "Could you help me"
+  line "with our research?"
+	done
+
+SilphCoCharlieText_ExitText:
+  text "This data is"
+  line "invaluable!"
+
+  para "Please, come back"
+  line "again tomorrow."
+  done
+
 SilphCo1F_MapEvents:
 	db 0, 0 ; filler
 
@@ -67,3 +97,4 @@ SilphCo1F_MapEvents:
 	def_object_events
 	object_event  4,  2, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SilphCoReceptionistScript, -1
 	object_event 13,  1, SPRITE_OFFICER, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, SilphCoOfficerScript, -1
+	object_event 11,  4, SPRITE_SCIENTIST, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, SilphCoCharlieScript, -1
